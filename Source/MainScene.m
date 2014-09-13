f34f0a55295e4a4707e022a1e60689094551aa13
Rootul <rootulp@gmail.com>
2014-09-13 12:07:37 -0400

    scroll screen with physics node, esentially follow Santa to the right

--- Source/MainScene.m
+++ Source/MainScene.m
@@ -10,8 +10,10 @@
 static const CGFloat scrollSpeed = 80.f;
 @implementation MainScene {
     CCSprite *_hero;
+    CCPhysicsNode *_physicsNode;
 }
 - (void)update:(CCTime)delta {
     _hero.position = ccp(_hero.position.x + delta * scrollSpeed, _hero.position.y);
+    _physicsNode.position = ccp(_physicsNode.position.x - (scrollSpeed *delta), _physicsNode.position.y);
 }
 @end
